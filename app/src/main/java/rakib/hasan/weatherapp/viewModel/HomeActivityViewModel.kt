b54da4c54e66c81593c059b8478040d7c6e74e4a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-29 17:01:45 +0600

    HomeActivityViewModel ADDED!

---
+++ app/src/main/java/rakib/hasan/weatherapp/viewModel/HomeActivityViewModel.kt
@@ -0,0 +1,9 @@
+package rakib.hasan.weatherapp.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class HomeActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+
+}
