3d9e3840c6c7ac0ecae649b822ed2e988a721648
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-29 18:35:58 +0600

    Location Found!

--- app/src/main/java/rakib/hasan/weatherapp/view/HomeActivity.kt
+++ app/src/main/java/rakib/hasan/weatherapp/view/HomeActivity.kt
@@ -3,12 +3,16 @@ package rakib.hasan.weatherapp.view
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import rakib.hasan.weatherapp.R
 import rakib.hasan.weatherapp.databinding.ActivityHomeBinding
 import rakib.hasan.weatherapp.services.utils.Constants
@@ -17,6 +21,8 @@ import java.util.jar.Manifest
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityHomeBinding
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+
 
     companion object{
         private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100;
@@ -26,14 +32,40 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getUserCurrentLocation();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        binding.homeActivityWeatherCallButton.setOnClickListener(View.OnClickListener { getUserCurrentLocation() })
     }
 
     private fun getUserCurrentLocation() {
         if(checkPermissions()){
             if (isUserLocationEnabled()){
                 // rest of code
-                Toast.makeText(applicationContext, "May day!", Toast.LENGTH_LONG).show()
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+                        // Got last known location. In some rare situations this can be null.
+                    if (location != null){
+                        binding.homeActivityWeatherTv.setText("Lat : " + location.latitude + "   |   " + "Long : " + location.longitude)
+                    }else{
+                        Toast.makeText(applicationContext, "Sorry, can't find your location!", Toast.LENGTH_LONG).show()
+                    }
+                }
+
             }else{
                 //open setting
                 Toast.makeText(applicationContext, "Please, turn on the location", Toast.LENGTH_LONG).show()
